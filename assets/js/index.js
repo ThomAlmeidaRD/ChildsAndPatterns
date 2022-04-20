67db4f910d497444d021444b3e63684a6e04e5ea
Thomas Almeida <contato.thomasalmeidard@gmail.com>
2022-04-19 21:44:49 -0300

    estrura base

---
+++ assets/js/index.js
@@ -0,0 +1,9 @@
+
+var cardOne = document.getElementById('card-1')
+var cardTwo = document.getElementById('card-2')
+var cardThree = document.getElementById('card-3')
+
+function displayPattern(card){
+    var pattern = card.parentNode
+    console.log(pattern)
+}
